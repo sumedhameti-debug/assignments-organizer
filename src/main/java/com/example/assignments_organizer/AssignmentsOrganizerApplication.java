7df7d394162f6f58a8263aca0487dae3981d0af7
Sumedh Sai Ameti <sumedh.ameti@gmail.com>
2026-01-06 13:11:14 -0500

    Gradle

---
+++ src/main/java/com/example/assignments_organizer/AssignmentsOrganizerApplication.java
@@ -0,0 +1,37 @@
+package com.example.assignments_organizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.Events;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
+
+@SpringBootApplication
+public class AssignmentsOrganizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentsOrganizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/assignments_organizer/AssignmentsOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignments_organizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentsOrganizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
